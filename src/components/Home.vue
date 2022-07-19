442e42392de330686cd91d8c47a7fa590da9afb9
Andrés Perdomo <alvarado0925@gmail.com>
2022-07-19 12:03:39 -0500

    Composición del componente Layout

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -3,17 +3,27 @@
     <template #header>
       <Header />
     </template>
+    <template #resume>
+      <Resume />
+    </template>
+    <template #movements>
+      <Movements />
+    </template>
   </Layout>
 </template>
 
 <script>
 import Header from "./Header.vue";
 import Layout from "./Layout.vue";
+import Resume from "./Resume/Index.vue";
+import Movements from "./Movements.vue";
 
 export default {
   components: {
     Header,
     Layout,
+    Resume,
+    Movements,
   },
 };
 </script>
